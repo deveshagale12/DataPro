d03388ccfe63bdc0d8a621a50ef20a6355303c5c
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-06-05 11:44:27 +0530

    import updated

--- src/main/java/com/DataPro/controller/FileController.java
+++ src/main/java/com/DataPro/controller/FileController.java
@@ -1,19 +1,15 @@
- package com.DataPro.controller;
-
+package com.DataPro.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import or
-g.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.DataPro.dto.FileDTO;
 import com.DataPro.entity.AccessRequest;
 import com.DataPro.service.FileService;
-
 import java.util.List;
 import java.util.Map;
 
